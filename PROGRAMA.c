ca4ffbdb5f4216658b56455697016c864380d519
HugoN <hugonogueron2607@gmail.com>
2024-01-18 17:26:21 -0600

    gfsddgf

--- PROGRAMA.c
+++ PROGRAMA.c
@@ -9,6 +9,12 @@
 typedef struct {
     char board[BOARD_SIZE][BOARD_SIZE];
     char player_board_1[BOARD_SIZE][BOARD_SIZE];
+    char player_board_2[BOARD_SIZE][BOARD_SIZE];
+    char game_board_1[BOARD_SIZE][BOARD_SIZE];
+    char game_board_2[BOARD_SIZE][BOARD_SIZE];
+    bool enemy_ships[BOARD_SIZE][BOARD_SIZE]={false};
+    bool enemy_ships_player_1[BOARD_SIZE][BOARD_SIZE]={false};
+    bool enemy_ships_player_2[BOARD_SIZE][BOARD_SIZE]={false};
     pthread_mutex_t mutex;
 } GameBoard;
 
@@ -18,20 +24,26 @@ GameBoard gameBoard;
 void init_board() {
     for (int i = 0; i < BOARD_SIZE; i++) {
         for (int j = 0; j < BOARD_SIZE; j++) {
-            gameBoard.board[i][j] = '-';
+            gameBoard.board[i][j] = '~';
         }
     }
 }
 
 // Función para imprimir el tablero
 void printBoard() {
+    printf("   ");
+        for (char c = 'A'; c < 'A' + BOARD_SIZE; c++) {
+            printf("%c ", c);
+        }
+    printf("\n");
+
     for (int i = 0; i < BOARD_SIZE; i++) {
+        printf("%2d ", i + 1);
         for (int j = 0; j < BOARD_SIZE; j++) {
             printf("%c ", gameBoard.board[i][j]);
         }
         printf("\n");
     }
-    printf("\n");
 }
 
 // Función para realizar la jugada de un jugador
@@ -40,30 +52,41 @@ void* playerMove(void* player) {
 
     while (1) {
         pthread_mutex_lock(&gameBoard.mutex);
+        
+        char x_char;
+        int y;
+        printf("Jugador %d, es tu turno.\n", *playerId);        
+        printf("Introduce las coordenadas (ejemplo, A5): ");
+        scanf("%c %d", &x_char, &y);
 
-        printf("Jugador %d, es tu turno.\n", *playerId);
-        int x, y;
-        printf("Introduce las coordenadas (fila columna): ");
-        scanf("%d %d", &x, &y);
+    int x = toupper(x_char) - 'A'; // Convertir letra a índice
+    y--;                           // Ajustar el índice a partir de 0
 
-        if (gameBoard.board[x][y] == '-') {
-            gameBoard.board[x][y] = *playerId + '0';
-            printf("¡Acertaste!\n");
-            printBoard();
+    if (x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE) {
+        if (gameBoard.enemy_ships[y][x]) {
+            printf("¡Impacto! Has alcanzado un barco enemigo en la posición (%c, "
+                "%d)!\n",
+                'A' + x, y + 1);
+            gameBoard.player_board[y][x] =
+                'X'; // Marcamos el impacto en el tablero del jugador
+            gameBoard.enemy_ships[y][x] = false; // "Hundimos" el barco enemigo
         } else {
-            printf("¡Agua!\n");
+            printf("Disparo al agua en la posición (%c, %d).\n", 'A' + x, y + 1);
+            gameBoard.player_board[y][x] =
+                'O'; // Marcamos el disparo al agua en el tablero del jugador
         }
-
-        pthread_mutex_unlock(&gameBoard.mutex);
-        sleep(1);
+    } else {
+        printf("Disparo fuera de los límites. Inténtalo de nuevo.\n");
     }
-
-    return NULL;
 }
 
 int main() {
     init_board(gameBoard.player_board_1);
-    printBoard();
+    init_board(gameBoard.player_board_2);
+    init_board(gameBoard.game_board_1);
+    init_board(gameBoard.game_board_2);
+    
+    printf("Bienvendio a Batalla Naval:\n")
 
     pthread_t player1, player2;
     int playerId1 = 1, playerId2 = 2;
